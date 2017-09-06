40ce219ecfc596c84e9b96713f7a71020330eabd
sjgraffjr <sjgraffjr@gmail.com>
2017-09-06 12:25:44 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -6,7 +6,29 @@ var cardFour = "king";
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardThree);
 */
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit:  "hearts",
+	cardImage: "images/queen-of-hearts.png",
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage:"images/queen-of-diamonds.png",
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png",
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png",
+},
+
+];
 var cardsInPlay = [];
 var checkForMatch = function() {
 	//if (cardsInPlay.length === 2){
@@ -18,8 +40,10 @@ else {
 	}
 }
 var flipcard = function(cardId) {
-console.log("User flipped " + cards[cardId])
-cardsInPlay.push(cards[cardId]);
+console.log("User flipped " + cards[cardId].rank);
+console.log("cardImage");
+console.log("suit");
+cardsInPlay.push(cards[cardId].rank);
 checkForMatch();
 }
 flipcard(0);
